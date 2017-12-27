55df31d6a6a33a1759445c97c46935d21e6c49e9
ytakemon <yuka.takemon@gmail.com>
2017-12-27 10:28:09 -0500

    Creating streamline method for plotting and indicating 15% drop in weight

--- Rfx3xCol4a5/WeeklyWeights.R
+++ Rfx3xCol4a5/WeeklyWeights.R
@@ -1,5 +1,40 @@
 # R/3.4.3 kite-eating tree
 library(ggplot2)
 library(tidyverse)
+library(lubridate)
+library(rlang)
 setwd("~/Desktop/1706 and 1713 Col4a5xRfx3/Data/")
+options(tibble.width = Inf)
 wweights <- read_csv("1713_weekly_weights.csv")
+
+# tidy dataframe
+wweights$DOB <- myd(wweights$DOB)
+wweights$Sex <- parse_factor(wweights$Sex, levels = unique(wweights$Sex))
+wweights$Col4a5_geno <- parse_factor(wweights$Col4a5_geno, levels = unique(wweights$Col4a5_geno))
+wweights$Rfx3_geno <- parse_factor(wweights$Rfx3_geno, levels = unique(wweights$Rfx3_geno))
+wweights$Cat <- parse_factor(wweights$Cat, levels = unique(wweights$Cat))
+
+# gather by weeks and prepare for plotting
+wks <- colnames(wweights)[10:ncol(wweights)]
+ggdata <- wweights %>%
+              gather(wks, key = "Week", value = "Weight")
+
+#plot
+ggplot(ggdata, aes(x = Week, y = Weight, group = Animal_ID, colour = Cat, shape = Cat)) +
+  geom_point() +
+  geom_line() +
+  labs( title = "1713 Col4a5xRfx3 Weekly Weights",
+        subtitle = "15% drop indicator for euthanization",
+        y = "Weights (g)",
+        x = "Weeks",
+        colour = "Cohorts",
+        shape = "Cohorts")
+
+# Check if any animals droped their weight by 15%
+wweights <- wweights %>%
+                  # Calculate by row
+                  rowwise() %>%
+                  # calculate the max per row of each
+                  mutate( drop15= max(!!! syms(wks)) * 0.85 > !!! syms(wks[[length(wks)]]))
+
+any(wweights$drop15, na.rm = TRUE)
