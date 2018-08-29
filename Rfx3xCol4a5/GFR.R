477cc2d6e6771611927cae40472360f4a5f25fac
ytakemon <yuka.takemon@gmail.com>
2018-08-29 15:54:14 -0400

    Added QC

--- Rfx3xCol4a5/GFR.R
+++ Rfx3xCol4a5/GFR.R
@@ -14,20 +14,44 @@ data_all <- NULL
 for(i in 1:length(files)){
   # Read in data
   data <- read_excel(files[i]) %>%
-            select(Line, Name, Sex, Cohort, Age, "Weight (g)", "Injection volume (ul)", "T1/2","GFR ul/min", "CI Min", "CI Max", "CI Difference", R2) %>%
+            select(Line, Name, Sex, Cohort, Age, "Weight (g)", "Injection volume (ul)", "T1/2","GFR ul/min", "CI Min", "CI Max", "CI Difference", "Peak", "Baseline","Peak to baseline ratio","R2") %>%
             rename(Weight = "Weight (g)",
                    Inj_vol = "Injection volume (ul)",
                    T_half = "T1/2",
                    GFR = "GFR ul/min",
                    CI_min = "CI Min",
                    CI_max = "CI Max",
-                   CI_diff = "CI Difference") %>%
+                   CI_diff = "CI Difference",
+                   Peak2BaseRatio = "Peak to baseline ratio") %>%
             filter(Name != is.na(Name) & Line == "1713") %>%
+            mutate(
+              Weight = as.numeric(Weight),
+              Inj_vol = as.numeric(Inj_vol),
+              T_half = as.numeric(T_half),
+              GFR = as.numeric(GFR),
+              CI_min = as.numeric(CI_min),
+              CI_max = as.numeric(CI_max),
+              CI_diff = as.numeric(CI_diff),
+              Peak2BaseRatio = as.numeric(Peak2BaseRatio),
+              R2 = as.numeric(R2)) %>%
             arrange(Name)
 
   # Add to the rest of the dataset
   data_all <- rbind(data_all, data) %>% arrange(Name)
 }
+
+# Run QC:
+data_all$QC_pass <- NA
+for( i in 1:nrow(data_all)){
+  if(is.na(data_all$CI_diff[i]) | is.na(data_all$Peak2BaseRatio[i])){
+    next
+  } else if(data_all$CI_diff[i] > 2 | data_all$Peak2BaseRatio[i] < 3){
+    data_all$QC_pass[i] <- FALSE
+  } else {
+    data_all$QC_pass[i] <- TRUE
+  }
+}
+data_all <- data_all[data_all$QC_pass %in% TRUE,]
 
 # Box plot and anova
 plot <- ggplot(data_all, aes(x= Cohort, y= GFR, fill = Cohort))+
